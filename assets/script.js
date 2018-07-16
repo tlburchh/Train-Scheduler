dd85c3053f68cbc160f8aaed9d21d82fc139a907
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-07-16 16:13:49 -0400

    Information can be submitted in UI, stored in firebox and returned to UI.

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,36 @@ var config = {
     // Create a variable to reference the database
     var database = firebase.database();
     var trainDatabase = database.ref(trainDatabase)
+
+    var trainName = "";
+    var destination = "";
+    var firstTrainTime = "";
+    var frequency ="";
+    $("#add-train").on("click", function(event){
+        event.preventDefault();
+        trainName = $("#train").val().trim();
+        destination = $("#destination").val().trim();
+        firstTrainTime = $("#firstTT").val().trim();
+        frequency = $("#frequency").val().trim();
+
+        trainDatabase.set({
+            trainName: trainName,
+            destination: destination,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency
+        });
+    });
+trainDatabase.on("value", function(snapshot){
+    console.log(snapshot.val());
+    console.log(snapshot.val().trainName);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().firstTrainTime);
+    console.log(snapshot.val().frequency);
+
+    $("#cTrain").text(snapshot.val().trainName);
+    $("#cDestination").text(snapshot.val().destination);
+    $("#cFrequency").text(snapshot.val().frequency);
+    $("#cArrival").text(snapshot.val().firstTrainTime);
+}, function(errorObjects) {
+    console.log("Errors handled: " + errorObject.code)
+});
