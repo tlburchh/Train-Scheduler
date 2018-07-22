e258a4a8431fc5adb26c23f377f6e1cf7cfc1fcc
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-07-22 16:48:03 -0400

    trying to clarify issues with updating next train time

--- assets/js.js
+++ assets/js.js
@@ -10,36 +10,39 @@ var config = {
 
     // Create a variable to reference the database
     var database = firebase.database();
-    var trainDatabase = database.ref("trainDatabase");
+    var trainDatabase = database.ref("trainDatabase")
 
     var trainName = "";
     var destination = "";
     var firstTrainTime = "00:00";
     var frequency = 0;
+    // var nextTrain = "00:00";
     // var minutes="";
 
     //setting variables for moment.js time manipulation
 
         // var tFrequency = 3;
-    
+
     // // Time is 3:30 AM
     // var firstTime = "03:30";
-    
+
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
     console.log(firstTimeConverted);
-    
+
     // Current Time
     var currentTime = moment();
     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-    
+
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     console.log("DIFFERENCE IN TIME: " + diffTime);
-    
+
     // Time apart (remainder)
-    console.log(typeof frequency);
-    console.log('freq ', frequency);
+    console.log(typeof frequency)
+    console.log('freq ', frequency)
+
+
     //click function that adds trains to list and adds info to Firebase
     $("#add-train").on("click", function(event){
         event.preventDefault();
@@ -48,52 +51,54 @@ var config = {
         firstTrainTime = $("#firstTT").val().trim();
         frequency = $("#frequency").val().trim();
         frequency = parseInt(frequency, 10);
-        // console.log(frequency);
-
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        nextTrainTime = $("nextTrain").val().trim();
         var tRemainder = diffTime % frequency;
         console.log(tRemainder);
         // Minute Until Train
         var tMinutesTillTrain = frequency - tRemainder;
         console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-        
+
         // Next Train
-        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        // return nextTrain;
         console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-        
+
+
+
+console.log(frequency);
+console.log(typeof frequency)
         trainDatabase.push({
             trainName: trainName,
             destination: destination,
             firstTrainTime: firstTrainTime,
-            nextTrain: nextTrain,
             frequency: frequency,
             tMinutesTillTrain: tMinutesTillTrain,
+            nextTrainTime: nextTrainTime
         });
-            console.log("next train: " + nextTrain);
-
-        $("#train").val("");
-        $("#destination").val("");
-        $("#firstTT").val("");
-        $("#frequency").val(0);
+        $("#train").val("")
+        $("#destination").val("")
+        $("#firstTT").val("")
+        $("#frequency").val(null)
+        
+        //concatenates information into table
     });
-
-//concatenates information into table
 trainDatabase.on("child_added", function(childSnapshot){
+  console.log('CHILD SNAPSHOT ', childSnapshot.val())
     $("tBody").append("<tr class='well'><td class='train-name'> " + childSnapshot.val().trainName +
     " </td><td class='train-destination'> " + childSnapshot.val().destination +
-      " </td><td class='train-frequency'> " + childSnapshot.val().frequency +
-      " </td><td class='first-train-time'> " + childSnapshot.val().nextTrain + 
+    " </td><td class='train-frequency'> " + childSnapshot.val().frequency +
+      " </td><td class='first-train-time'> " + childSnapshot.val().nextTrainTime +
       " </td><td class='train-minutes'> " +  childSnapshot.val().tMinutesTillTrain + " </td></tr>");
-      
+
     });
-        
+
         // function (errorObjects) {
             //     console.log("Errors handled: " + errorObject.code);
-            
+
             // }
-            
+
 
 //adds new listing to table
 trainDatabase.orderByChild("dataAdded").limitToLast(1).on("child_added", function(snapshot) {
 
 });
-
